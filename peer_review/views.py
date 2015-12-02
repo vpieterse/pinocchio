83709364929e08fa661d810f16ab4a87bccf01bc
nigelmpofu <nvmpofu@gmail.com>
2015-12-02 15:58:45 +0200

    Typecasting in questionnaire

--- peer_review/views.py
+++ peer_review/views.py
@@ -81,7 +81,7 @@ def questionnaire(request, questionnairePk):
 	if request.method == "POST":
 		context = {'questionnaire': Questionnaire.objects.all(), 'questions' : Question.objects.all(),
 				'questionTypes' : QuestionType.objects.all(), 'questionOrder' : QuestionOrder.objects.all(),
-				'questionGrouping' : QuestionGrouping.objects.all(), 'questionnairePk' : questionnairePk}
+				'questionGrouping' : QuestionGrouping.objects.all(), 'questionnairePk' : int(questionnairePk)}
 		return render(request, 'peer_review/questionnaire.html', context)
 	else:
 		return render(request, 'peer_review/userError.html')
