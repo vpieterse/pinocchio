b1aab19c586ada516a183bda56e979013dda159d
Matthias <matthiasharvey@gmail.com>
2016-01-26 11:02:45 +0200

    Updated User model to have a OneToOne relationship with UserDetail

--- peer_review/models.py
+++ peer_review/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-from django.db import models
 from django.core.management import call_command
+from django.db import models
 from django.utils import timezone
 
 
@@ -50,6 +50,7 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choiceText
+
 
 class FreeformItem(models.Model):
     question = models.ForeignKey(Question)
@@ -101,7 +102,10 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     OTP = models.BooleanField(default=True)
     status = models.CharField(max_length=1)
-    userDetail = models.ForeignKey(UserDetail)
+    userDetail = models.OneToOneField(
+            UserDetail,
+            on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.userId + " - " + self.userDetail.surname + " " + self.userDetail.initials
@@ -118,6 +122,7 @@ class QuestionOrder(models.Model):
     question = models.ForeignKey(Question)
     order = models.IntegerField(default=1)
 
+
 class RoundDetail(models.Model):
     questionnaire = models.ForeignKey(Questionnaire)
     startingDate = models.DateTimeField('starting date')
@@ -126,6 +131,7 @@ class RoundDetail(models.Model):
 
     def __str__(self):
         return self.description
+
 
 class TeamDetail(models.Model):
     userDetail = models.ForeignKey(UserDetail, null=True)
@@ -142,4 +148,4 @@ class TeamDetail(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=NOT_ATTEMPTED)
 
     def __str__(self):
-        return self.roundDetail.description + " " + self.teamName + " (" + self.userDetail.surname + ", " +self.userDetail.initials + ")"
+        return self.roundDetail.description + " " + self.teamName + " (" + self.userDetail.surname + ", " + self.userDetail.initials + ")"
