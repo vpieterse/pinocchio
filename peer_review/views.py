996c32399438fc4189cbb4999851766f1cb51de6
MatthewBotha <m.botha41@gmail.com>
2016-07-14 10:50:04 +0200

    Maintain Team Refactoring complete

--- peer_review/views.py
+++ peer_review/views.py
@@ -10,7 +10,6 @@ from django.contrib.auth import authenticate, login as django_login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from django.http import HttpResponseRedirect
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.shortcuts import render_to_response
@@ -25,6 +24,7 @@ from .models import User
 # Moved these views into seperate files
 from .view.questionAdmin import question_admin, edit_question, save_question, delete_question
 from .view.questionnaireAdmin import questionnaire_admin, edit_questionnaire, save_questionnaire, delete_questionnaire
+from .view.maintainTeam import maintain_team, change_team_status, change_user_team_for_round, get_teams_for_round, get_teams
 
 
 def active_rounds(request):
@@ -121,24 +121,6 @@ def maintain_round(request):
     context = {'roundDetail': RoundDetail.objects.all(),
                'questionnaires': Questionnaire.objects.all()}
     return render(request, 'peer_review/maintainRound.html', context)
-
-
-def maintain_team(request):
-    if request.method == "POST":
-        round_pk = request.POST.get("roundPk")
-
-        context = {'users': User.objects.all(),
-                   'rounds': RoundDetail.objects.all(),
-                   'teams': TeamDetail.objects.all(),
-                   'roundPk': round_pk}
-
-    else:
-        context = {'users': User.objects.all(),
-                   'rounds': RoundDetail.objects.all(),
-                   'teams': TeamDetail.objects.all(),
-                   'roundPk': "none"}
-    return render(request, 'peer_review/maintainTeam.html', context)
-
 
 # def questionAdmin(request):
 #     # print(request.user.is_authenticated())
@@ -284,38 +266,6 @@ def user_list(request):
     return render(request, 'peer_review/userAdmin.html',
                   {'users': users, 'userForm': user_form, 'docForm': doc_form, 'email_text': email_text})
 
-
-def get_teams(request):
-    response = {}
-    if request.method == "GET":
-        teams = TeamDetail.objects.all()
-        for team in teams:
-            user = User.objects.get(pk=team.user.pk)
-            response[team.pk] = {
-                'userId': user.userId,
-                'initials': team.user.initials,
-                'surname': team.user.surname,
-                'round': team.roundDetail.name,
-                'team': team.teamName,
-                'status': team.status,
-                'teamId': team.pk,
-            }
-    elif request.method == "POST":
-        user_pk = request.POST.get("pk")
-        user = User.objects.get(pk=user_pk)
-
-        teams = TeamDetail.objects.filter(user=user)
-        for team in teams:
-            response[team.pk] = {
-                'round': team.roundDetail.name,
-                'team': team.teamName,
-                'status': team.status,
-                'teamId': team.pk,
-                'roundPk': team.roundDetail.pk
-            }
-    return JsonResponse(response)
-
-
 def get_questionnaire_for_round(request, round_pk):
     round = RoundDetail.objects.get(pk=round_pk)
     if request.method == "GET":
@@ -323,42 +273,6 @@ def get_questionnaire_for_round(request, round_pk):
             'questionnaire': round.questionnaire.label
         }
     return JsonResponse(response)
-
-
-def get_teams_for_round(request, round_pk):
-    teams = TeamDetail.objects.filter(roundDetail_id=round_pk)
-    response = {}
-    for team in teams:
-        response[team.pk] = {
-            'userId': team.user.pk,
-            'teamName': team.teamName,
-            'status': team.status,
-        }
-    # print(response)
-    return JsonResponse(response)
-
-
-def change_user_team_for_round(request, round_pk, user_pk, team_name):
-    try:
-        team = TeamDetail.objects.filter(user_id=user_pk).get(roundDetail_id=round_pk)
-    except TeamDetail.DoesNotExist:
-        team = TeamDetail(
-            user=User.objects.get(pk=user_pk),
-            roundDetail=RoundDetail.objects.get(pk=round_pk)
-        )
-    team.teamName = team_name
-    if team_name == 'emptyTeam':
-        team.status = 'NA'
-    team.save()
-    return JsonResponse({'success': True})
-
-
-def change_team_status(request, team_pk, status):
-    team = TeamDetail.objects.get(pk=team_pk)
-    team.status = status
-    team.save()
-    return JsonResponse({'success': True})
-
 
 def generate_otp():
     n = random.randint(4, 10)
